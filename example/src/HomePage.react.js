77860d66cc962989336d3552ec8f6b24bdb71b35
Alex Cui <alexbroguy@gmail.com>
2019-01-17 02:27:08 -0500

    Restructure statistics calculations, and add reactstrap

--- example/src/HomePage.react.js
+++ example/src/HomePage.react.js
@@ -4,6 +4,8 @@ import * as React from "react";
 
 import { Page, Grid, Alert, StatsCard, Progress } from "tabler-react";
 
+import {Card, CardTitle} from "reactstrap";
+
 import SiteWrapper from "./SiteWrapper.react";
 
 type Props = {
@@ -13,86 +15,108 @@ type Props = {
 class Home extends React.PureComponent<Props, void> {
   constructor(props) {
     super(props);
+
+    this.crimeRatio = {
+      murder: 0.1,
+      rape: 0.2,
+      sexualAssault: 0.2,
+      assault: 0.2,
+      kidnap: 0.2,
+      homeBurnt: 0.1,
+    };
+
+    this.totalCrimeTime = 18 * 60;
   }
 
   render() {
+    var secondsPassed = this.props.time / 10;
+    var statsList = {
+      murder: secondsPassed/(this.totalCrimeTime / this.crimeRatio.murder),
+      rape: secondsPassed/(this.totalCrimeTime / this.crimeRatio.rape),
+      sexualAssault: secondsPassed/(this.totalCrimeTime / this.crimeRatio.sexualAssault),
+      assault: secondsPassed/(this.totalCrimeTime / this.crimeRatio.assault),
+      kidnap: secondsPassed/(this.totalCrimeTime / this.crimeRatio.kidnap),
+      homeBurnt: secondsPassed/(this.totalCrimeTime / this.crimeRatio.homeBurnt),
+    };
+
     return (
       <SiteWrapper>
         <Page.Content>
           <Grid.Row cards={true}>
             <Grid.Col md={12}>
-              <Alert type="primary">
-                It has been {Math.round(this.props.time/10)} seconds.
-              </Alert>
+              <Card body inverse color="danger">
+                <CardTitle>It has been {Math.round(secondsPassed)} seconds.</CardTitle>
+              </Card>
             </Grid.Col>
             <Grid.Col width={6} sm={4} lg={2}>
               <Progress size="xs">
-                <Progress.Bar color="red" width={Math.round(this.props.time) % 100} />
+                <Progress.Bar color="red" width={Math.round(statsList.murder % 1 * 100)} />
               </Progress>
               <StatsCard
                 layout={1}
                 movement={300}
-                total={Math.floor(this.props.time / 100)}
+                total={Math.floor(statsList.murder)}
                 label="Dalits were murdered"
               />
             </Grid.Col>
             <Grid.Col width={6} sm={4} lg={2}>
               <Progress size="xs">
-                <Progress.Bar color="red" width={Math.round(this.props.time * 2) % 100} />
+                <Progress.Bar color="red" width={Math.round(statsList.assault % 1 * 100)} />
               </Progress>
               <StatsCard
+                body inverse color="danger"
                 layout={1}
                 movement={350}
-                total={Math.floor(this.props.time / 50)}
+                total={Math.floor(statsList.assault)}
                 label="Dalits were assaulted"
               />
             </Grid.Col>
             <Grid.Col width={6} sm={4} lg={2}>
               <Progress size="xs">
-                <Progress.Bar color="red" width={Math.round(this.props.time /3) % 100} />
+                <Progress.Bar color="red" width={Math.round(statsList.rape % 1 * 100)} />
               </Progress>
 
               <StatsCard
                 layout={1}
                 movement={250}
-                total={Math.floor(this.props.time / 300)}
+                total={Math.floor(statsList.rape)}
                 label="Dalits were raped"
               />
             </Grid.Col>
             <Grid.Col width={6} sm={4} lg={2}>
               <Progress size="xs">
-                <Progress.Bar color="red" width={Math.round(this.props.time * 10/9) % 100} />
+                <Progress.Bar color="red" width={Math.round(statsList.kidnap % 1 * 100)} />
               </Progress>
 
               <StatsCard
                 layout={1}
                 movement={300}
-                total={Math.floor(this.props.time / 90)}
+                total={Math.floor(statsList.kidnap)}
                 label="Dalits were kidnapped"
               />
             </Grid.Col>
             <Grid.Col width={6} sm={4} lg={2}>
               <Progress size="xs">
-                <Progress.Bar color="red" width={Math.round(this.props.time /4) % 100} />
+                <Progress.Bar color="red" width={Math.round(statsList.homeBurnt % 1 * 100)} />
               </Progress>
 
               <StatsCard
                 layout={1}
                 movement={200}
-                total={Math.floor(this.props.time / 400)}
+                total={Math.floor(statsList.homeBurnt)}
                 label="Dalit homes were burnt"
               />
             </Grid.Col>
             <Grid.Col width={6} sm={4} lg={2}>
               <Progress size="xs">
-                <Progress.Bar color="red" width={Math.round(this.props.time /2) % 100} />
+                <Progress.Bar color="red" width={Math.round(statsList.sexualAssault % 1 * 100)} />
               </Progress>
 
               <StatsCard
                 layout={1}
                 movement={100}
-                total={Math.floor(this.props.time / 200)}
-                label="Dalits were denied healthcare"
+                total={Math.floor(statsList.sexualAssault)}
+                label="Dalits were sexually assaulted"
               />
             </Grid.Col>
           </Grid.Row>
